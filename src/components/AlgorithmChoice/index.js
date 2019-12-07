34c21a03e4259af92e3e26a3494bfd1087b61c98
Yatin M <iamyatin@gmail.com>
2019-12-07 22:31:57 +0530

    Change Title

--- src/components/AlgorithmChoice/index.js
+++ src/components/AlgorithmChoice/index.js
@@ -1,13 +1,15 @@
-// import React from "react";
-// import { useDispatch } from "react-redux";
-// import Form from "react-bootstrap/Form";
+import React from "react";
+import { useDispatch } from "react-redux";
+import Form from "react-bootstrap/Form";
 
-// const AlgorithmChoice = () => {
+const AlgorithmChoice = () => {
+  const dispatch = useDispatch();
 
-//     const dispatch = useDispatch();
+  return (
+    <div className="py-3">
+      <Form.Check inline label="2" type="radio" id="" />
+    </div>
+  );
+};
 
-//     return <div className="py-3">
-//       <Form.Check inline label="2" type="radio" id="" />
-
-//     </div>
-// }
+export default AlgorithmChoice;
