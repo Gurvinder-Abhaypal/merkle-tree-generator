0625f860d6a666e4c297a2a762564111b53fb6bd
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2019-12-13 21:19:13 +0530

    Move to material ui from bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from "react";
 import "App.css";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Tabs from "react-bootstrap/Tabs";
-import Tab from "react-bootstrap/Tab";
-import Button from "react-bootstrap/Button";
 import { useDispatch } from "react-redux";
 import { useSelector } from "utils/TypedUseSelector";
 import JSONPretty from "react-json-pretty";
-import Jumbotron from "react-bootstrap/Jumbotron";
+
+import {
+  Container,
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  Grid,
+  Tabs,
+  Tab,
+  Box
+} from "@material-ui/core";
 
 import DirectInput from "components/DirectInput";
 import { alterInputMethodAction } from "store/app/actions";
+import SwipeableViews from "react-swipeable-views";
 //@ts-ignore
 let merkle = require("merkle-tree-gen");
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
 
 function App() {
   const inputMethod = useSelector(state => state.app.inputMethod);
   const globalInputs = useSelector(state => state.app.inputs);
-  const dispatch = useDispatch();
 
   const [merkleTree, setMerkleTree] = React.useState("");
   console.log(inputMethod);
@@ -43,65 +69,104 @@ function App() {
       }
     });
   };
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = React.useCallback(
+    (event: React.ChangeEvent<{}>, newValue: number) => {
+      setValue(newValue);
+    },
+    [value]
+  );
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
   return (
     <div className="App">
-      <Container fluid={false}>
-        <Navbar className="border-bottom" bg="transparent" expand="lg">
-          <Navbar.Brand>Merkle Tree Generator</Navbar.Brand>
-          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-              <a className="nav-link" href="/">
-                <img
-                  src="https://block8.com/images/home/block8-icon.svg"
-                  alt="Block"
-                />
-              </a>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <AppBar position="static" style={{ marginBottom: "20px" }}>
+        <Toolbar>
+          <Typography variant="h5">Merkle Tree Generator</Typography>
+          <a className="nav-link" href="/">
+            <img
+              src="https://block8.com/images/home/block8-icon.svg"
+              alt="Block"
+            />
+          </a>
+        </Toolbar>
+      </AppBar>
 
-        <Jumbotron fluid className="bg-transparent py-4 my-4">
-          <Container>
-            <h1>Merkle Tree Generator Online</h1>
-            <p>
-              <a
-                href="https://en.wikipedia.org/wiki/Merkle_tree"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                What's a merkle Tree ?
-              </a>
-            </p>
-          </Container>
-        </Jumbotron>
-      </Container>
+      <Grid>
+        <Grid style={{ padding: "2em" }}>
+          <Typography align="left" variant="h4" gutterBottom>
+            Generate a merkle tree online
+          </Typography>
+          <Typography align="left">
+            <a
+              href="https://en.wikipedia.org/wiki/Merkle_tree"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              What's a merkle Tree ?
+            </a>
+          </Typography>
+        </Grid>
 
-      <Container className="" style={{ padding: "0 1.3rem" }}>
-        <Tabs
-          defaultActiveKey="direct-input"
-          id="uncontrolled-tab-example"
-          onSelect={(k: string) => dispatch(alterInputMethodAction(k))}
-        >
-          <Tab eventKey="direct-input" title="Direct Input">
-            <DirectInput />
-          </Tab>
-          <Tab eventKey="via-file" title="Via File">
-            Upload File
-          </Tab>
-        </Tabs>
+        <Grid container justify="center" style={{ padding: "2em 0em" }}>
+          <Grid
+            item
+            sm={5}
+            style={{
+              border: "2px solid grey",
+              borderRadius: "5px"
+            }}
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="simple tabs example"
+            >
+              <Tab label="Direct Input" />
+              <Tab label="Upload File" />
+            </Tabs>
 
-        <div className="py-4"></div>
-        <div className="py-4">
-          <Button variant="primary" onClick={() => createMerkleTree()}>
-            Generate
-          </Button>
-        </div>
-
-        <div>
-          <JSONPretty id="json-pretty" data={merkleTree}></JSONPretty>
-        </div>
-      </Container>
+            <SwipeableViews
+              axis={"x"}
+              index={value}
+              onChangeIndex={handleChangeIndex}
+            >
+              <TabPanel value={value} index={0}>
+                <DirectInput />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                Upload File
+              </TabPanel>
+            </SwipeableViews>
+          </Grid>
+          <Grid item container justify="center" alignItems="center" sm={1}>
+            <Button
+              style={{ height: "100%", margin: "0em 0.5em" }}
+              color="primary"
+              variant="outlined"
+              onClick={() => createMerkleTree()}
+            >
+              Generate
+            </Button>
+          </Grid>
+          <Grid
+            item
+            sm={5}
+            style={{
+              border: "2px solid grey",
+              borderRadius: "5px",
+              maxHeight: "500px"
+            }}
+          >
+            <JSONPretty id="json-pretty" data={merkleTree}></JSONPretty>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/components/AlgorithmChoice/index.tsx
+++ src/components/AlgorithmChoice/index.tsx
@@ -3,8 +3,6 @@ import { useDispatch } from "react-redux";
 import Form from "react-bootstrap/Form";
 
 const AlgorithmChoice = () => {
-  const dispatch = useDispatch();
-
   return (
     <div className="py-3">
       <Form.Check inline label="2" type="radio" id="" />
